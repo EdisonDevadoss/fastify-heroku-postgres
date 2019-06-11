d8e8f6cde5eb61d960a31019e82287c2ae01657f
Edison Devadoss <edisonj1996@gmail.com>
2019-06-11 12:55:54 +0530

    Undo code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const fastify = require('fastify')({
   logger: true
 });
-// const jsonParser = require('fast-json-body');
+const jsonParser = require('fast-json-body');
 
 const db = require('./queries');
 
 const PORT = process.env.NODE_ENV || 3000;
 
-// fastify.addContentTypeParser('application/jsoff', function (req, done) {
-//   jsonParser(req, function (err, body) {
-//     done(err, body)
-//   })
-// });
+fastify.addContentTypeParser('application/jsoff', function (req, done) {
+  jsonParser(req, function (err, body) {
+    done(err, body)
+  })
+});
 
 fastify.get('/', (req, res)=>{
     res.send({hello: 'world'})
 })
 
-// fastify.get('/users', db.getUsers);
-// fastify.get('/users/:id', db.getUserById);
-// fastify.post('/users/', db.createUser);
-// fastify.put('/users/:id', db.updateUser);
-// fastify.delete('/users/:id', db.deleteUser);
+fastify.get('/users', db.getUsers);
+fastify.get('/users/:id', db.getUserById);
+fastify.post('/users/', db.createUser);
+fastify.put('/users/:id', db.updateUser);
+fastify.delete('/users/:id', db.deleteUser);
 
 fastify.listen(PORT)
